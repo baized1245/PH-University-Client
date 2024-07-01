a81242d557bae8a4c7678f7617ab67cbcb314dab
unknown <you@example.com>
2024-07-01 19:44:33 +0600

    module-27.6

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,46 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
-import AdminLayout from "../components/layout/AdminLayout";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
   {
     path: "/admin",
-    element: <AdminLayout />,
-    children: [
-      {
-        // path: "dashboard",
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-    ],
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminPaths,
   },
   {
     path: "register",
