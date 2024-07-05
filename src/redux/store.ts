b0a6af44c593efa627c84db350cca7e9722e1a88
unknown <you@example.com>
2024-07-05 16:45:26 +0600

    module-28.8

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authSlice from './features/auth/authSlice'
+import authReduce from './features/auth/authSlice'
 import { baseApi } from './api/baseApi'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'auth',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReduce)
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
-        auth: authSlice
+        auth: persistedReducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(baseApi.middleware)
@@ -17,3 +26,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+
+export const persistor = persistStore(store)
