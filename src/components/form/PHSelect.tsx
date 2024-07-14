20a5fdfdd3bef1bce1bfde8ada1a9ee8a2a0a0dc
unknown <you@example.com>
2024-07-14 08:57:51 +0600

    module-31.3

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -1,18 +1,22 @@
 import { Form, Select } from "antd";
+import { Controller } from "react-hook-form";
 
-const PHSelect = ({ label }) => {
+type TPHSelectProps = {
+  label: string;
+  name: string;
+  options: { value: string; label: string; disabled?: boolean }[];
+};
+
+const PHSelect = ({ label, name, options }: TPHSelectProps) => {
   return (
-    <Form.Item label={label}>
-      <Select
-        style={{ width: "100%" }}
-        options={[
-          { value: "jack", label: "Jack" },
-          { value: "lucy", label: "Lucy" },
-          { value: "Yiminghe", label: "yiminghe" },
-          { value: "disabled", label: "Disabled", disabled: true },
-        ]}
-      />
-    </Form.Item>
+    <Controller
+      name={name}
+      render={({ field }) => (
+        <Form.Item label={label}>
+          <Select style={{ width: "100%" }} {...field} options={options} />
+        </Form.Item>
+      )}
+    />
   );
 };
 
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,6 +1,5 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
 import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 
@@ -9,13 +8,26 @@ const CreateAcademicSemester = () => {
     console.log(data);
   };
 
+  const nameOptions = [
+    {
+      value: "01",
+      label: "Autumn",
+    },
+    {
+      value: "02",
+      label: "Summer",
+    },
+    {
+      value: "03",
+      label: "Fall",
+    },
+  ];
+
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="name" />
-          <PHInput type="text" name="name" label="year" />
-          <PHSelect label="name" />
+          <PHSelect label="name" name="name" options={nameOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
